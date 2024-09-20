d5bd6cf11b4d1256d464f2555d199fa39b9742f8
Chizaram <chizaramonuorah50@gmail.com>
2024-09-20 03:32:00 +0100

    fix: session time out

--- lib/view/features/auth/vm/login_vm.dart
+++ lib/view/features/auth/vm/login_vm.dart
@@ -4,6 +4,8 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:remittance_mobile/core/di/injector.dart';
 import 'package:remittance_mobile/core/storage/share_pref.dart';
 import 'package:remittance_mobile/data/models/requests/login_req.dart';
+import 'package:remittance_mobile/view/utils/session-manager/session_timeout_manager.dart';
+import 'package:remittance_mobile/view/utils/session-manager/vm/app_session_vm.dart';
 
 class LoginNotifier extends AutoDisposeAsyncNotifier<String> {
   Future<void> loginMethod(LoginReq req) async {
@@ -12,6 +14,7 @@ class LoginNotifier extends AutoDisposeAsyncNotifier<String> {
         () => ref.read(authRepository).loginEndpoint(req));
 
     if (!state.hasError) {
+      ref.read(sessionStateProvider).add(SessionState.startListening);
       SharedPrefManager.isLoggedIn = true;
       SharedPrefManager.isFirstLaunch = false;
     }
